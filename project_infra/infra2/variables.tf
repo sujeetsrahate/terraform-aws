8e43c32281da7ba3783c945ece422c4feba13533
Ashutosh Sanghi <ashutoshsanghi3@gmail.com>
2025-05-07 08:04:23 +0000

    creating infrastructure with variables

---
+++ project_infra/infra2/variables.tf
@@ -0,0 +1,81 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability zone"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name tag for VPC"
+  type        = string
+}
+
+variable "igw_name" {
+  description = "Name tag for Internet Gateway"
+  type        = string
+}
+
+variable "public_subnet_name" {
+  description = "Name tag for Public Subnet"
+  type        = string
+}
+
+variable "private_subnet_name" {
+  description = "Name tag for Private Subnet"
+  type        = string
+}
+variable "public_rt_name" {
+  description = "Name tag for Public Route Table"
+  type        = string
+}
+
+variable "private_rt_name" {
+  description = "Name tag for Private Route Table"
+  type        = string
+}
+variable "sg_name" {
+  description = "Name tag for Security Group"
+  type        = string
+}
+
+variable "public_ec2_name" {
+  description = "Name tag for Public EC2 instance"
+  type        = string
+}
+
+variable "private_ec2_name" {
+  description = "Name tag for Private EC2 instance"
+  type        = string
+}
+variable "natgw_name" {
+  description = "Name tag for NAT Gateway"
+  type        = string
+}
